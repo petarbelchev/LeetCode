5a8316f5a5d501939601f3384ff431cbfa1a10b8
Petar Belchev <petar9004@gmail.com>
2023-08-26 22:37:27 +0300

    Added Merge Sorted Array task solution

---
+++ TopInterview150/Tests/MergeSortedArrayTests.cs
@@ -0,0 +1,27 @@
+using Newtonsoft.Json;
+using TopInterview150;
+
+namespace Tests
+{
+	public class MergeSortedArrayTests
+	{
+		[Test]
+		[TestCase(new int[6] { 1, 2, 3, 0, 0, 0 }, 3, new int[3] { 2, 5, 6 }, 3, new int[6] { 1, 2, 2, 3, 5, 6 })]
+		[TestCase(new int[1] { 1 }, 1, new int[0], 0, new int[1] { 1 })]
+		[TestCase(new int[1] { 0 }, 0, new int[1] { 1 }, 1, new int[1] { 1 })]
+		[TestCase(new int[6] { 4, 5, 6, 0, 0, 0 }, 3, new int[3] { 1, 2, 3 }, 3, new int[6] { 1, 2, 3, 4, 5, 6 })]
+		[TestCase(new int[6] { 4, 5, 6, 0, 0, 0 }, 3, new int[3] { 1, 2, 7 }, 3, new int[6] { 1, 2, 4, 5, 6, 7 })]
+		public void MergeSortedArray_TestOne(int[] nums1, int m, int[] nums2, int n, int[] expected)
+		{
+			//Arrange
+			var mergeSortedArray = new MergeSortedArray();
+
+			//Act
+			mergeSortedArray.Merge(nums1, m, nums2, n);
+
+			//Assert
+			Assert.That(JsonConvert.SerializeObject(nums1),
+				Is.EqualTo(JsonConvert.SerializeObject(expected)));
+		}
+	}
+}
