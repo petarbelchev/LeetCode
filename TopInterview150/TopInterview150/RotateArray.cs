0451f13e4f863544a7d65e4446e568381a093017
Petar Belchev <petar9004@gmail.com>
2023-08-31 19:06:04 +0300

    Added the fastest Rotate Array task solution according to the LeetCode chart.

---
+++ TopInterview150/TopInterview150/RotateArray.cs
@@ -0,0 +1,27 @@
+﻿namespace TopInterview150;
+
+public class RotateArray
+{
+	public void Rotate(int[] nums, int k)
+	{
+		if (k == 0 || nums.Length == 1 || nums.Length == k)
+			return;
+
+		int s = 0;
+		int p = 0;
+		int e = nums[p];
+
+		for (int c = 1; c <= nums.Length; c++)
+		{
+			p = (p + k) % nums.Length;
+			(e, nums[p]) = (nums[p], e);
+
+			if (p == s)
+			{
+				s++;
+				p++;
+				e = nums[p];
+			}
+		}
+	}
+}
