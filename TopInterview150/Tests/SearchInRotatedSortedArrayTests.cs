3abcba3ea8083fb753c60713cc6543d8f4f0b7d3
Petar Belchev <petar.belchev@outlook.com>
2024-02-08 10:56:16 +0200

    Add Search in Rotated Sorted Array task solution

---
+++ TopInterview150/Tests/SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests;
+
+internal class SearchInRotatedSortedArrayTests
+{
+    [Test]
+    [TestCase(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+    [TestCase(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+    [TestCase(new int[] { 1 }, 0, -1)]
+    [TestCase(new int[] { 1, 3 }, 3, 1)]
+    [TestCase(new int[] { 5, 1, 3 }, 5, 0)]
+    [TestCase(new int[] { 7, 8, 1, 2, 3, 4, 5, 6 }, 2, 3)]
+    [TestCase(new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 8, 4)]
+    public void Tests(int[] nums, int target, int expected)
+    {
+        // Arrange
+        var instance = new SearchInRotatedSortedArray();
+
+        // Act
+        int actual = instance.Search(nums, target);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/SearchInRotatedSortedArray.cs
@@ -0,0 +1,56 @@
+﻿namespace TopInterview150;
+
+public class SearchInRotatedSortedArray
+{
+    public int Search(int[] nums, int target)
+    {
+        if (nums[0] > nums[^1])
+        {
+            int boundary = FindBoundary(nums);
+
+            if (target < nums[0])
+                return FindTargetIndex(nums, boundary + 1, nums.Length - 1, target);
+
+            return FindTargetIndex(nums, 0, boundary, target);
+        }
+
+        return FindTargetIndex(nums, 0, nums.Length - 1, target);
+    }
+
+    private int FindBoundary(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < nums[left])
+                right = mid - 1;
+            else if (nums[mid] < nums[mid + 1])
+                left = mid + 1;
+            else if (mid == 0 || (nums[mid] > nums[mid + 1]))
+                return mid;
+        }
+
+        return -1;
+    }
+
+    private int FindTargetIndex(int[] nums, int left, int right, int target)
+    {
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
