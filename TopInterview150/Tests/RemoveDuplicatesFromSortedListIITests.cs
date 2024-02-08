2e13c2b2abfa908176dfaf6cbef6fa325741f3a5
Petar Belchev <petar.belchev@outlook.com>
2024-02-08 18:35:48 +0200

    Add Remove Duplicates from Sorted List II task solution

---
+++ TopInterview150/Tests/RemoveDuplicatesFromSortedListIITests.cs
@@ -0,0 +1,50 @@
+﻿namespace Tests;
+
+internal class RemoveDuplicatesFromSortedListIITests
+{
+    [Test]
+    [TestCase(new int[] { 1, 2, 3, 3, 4, 4, 5 }, new int[] { 1, 2, 5 })]
+    [TestCase(new int[] { 1, 1, 1, 2, 3 }, new int[] { 2, 3 })]
+    [TestCase(new int[] { 1, 1 }, null)]
+    public void Tests(int[] input, int[] expected)
+    {
+        // Arrange
+        var instance = new RemoveDuplicatesFromSortedListII();
+        ListNode? head = GetHead(input);
+        ListNode? expectedHead = GetHead(expected);
+
+        // Act
+        ListNode actual = instance.DeleteDuplicates(head);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            while (actual != null && expectedHead != null)
+            {
+                Assert.That(actual.val, Is.EqualTo(expectedHead.val));
+                actual = actual.next;
+                expectedHead = expectedHead.next;
+            }
+
+            Assert.That(actual, Is.Null);
+            Assert.That(expectedHead, Is.Null);
+        });
+    }
+
+    private ListNode? GetHead(int[] input)
+    {
+        if (input == null)
+            return null;
+
+        ListNode head = new(input[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            current.next = new ListNode(input[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+}
---
+++ TopInterview150/TopInterview150/RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,64 @@
+﻿#nullable disable
+namespace TopInterview150;
+
+public class RemoveDuplicatesFromSortedListII
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head == null)
+            return null;
+
+        ListNode current = head;
+        ListNode next = head.next;
+
+        while (next != null)
+        {
+            if (current.val != next.val && next.val != next.next?.val)
+            {
+                current = next;
+                next = current.next;
+            }
+            else
+            {
+                int oldVal = next.val;
+                next = FindNewNext(next);
+
+                if (current.val != oldVal)
+                {
+                    current.next = next;
+                }
+                else
+                {
+                    head = next;
+                    current = head;
+                    next = next?.next;
+                }
+            }
+        }
+
+        return head;
+    }
+
+    private ListNode FindNewNext(ListNode node)
+    {
+        ListNode next = node.next;
+
+        while (next?.val == node.val)
+            next = next.next;
+
+        return next;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
