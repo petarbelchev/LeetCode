48419d185cc2f80eefd5cb6981569cdfe0f68f7a
Petar Belchev <petar.belchev@outlook.com>
2024-02-09 13:09:29 +0200

    Add Roman to Integer task solution

---
+++ TopInterview150/TopInterview150/RomanToInteger.cs
@@ -0,0 +1,34 @@
+﻿namespace TopInterview150;
+
+public class RomanToInteger
+{
+    public int RomanToInt(string s)
+    {
+        int right = GetIntValue(s.Last());
+        int integer = right;
+
+        for (int i = s.Length - 2; i >= 0; i--)
+        {
+            int left = GetIntValue(s[i]);
+
+            if (left < right) integer -= left;
+            else integer += left;
+
+            right = left;
+        }
+
+        return integer;
+    }
+
+    int GetIntValue(char ch) => ch switch
+    {
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+        _ => throw new ArgumentException("The Roman numeral contains an invalid symbol.")
+    };
+}
