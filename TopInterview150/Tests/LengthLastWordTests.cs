5a7090103f466e94463b65a4c1ddcfbaf9b901bd
Petar Belchev <petar.belchev@outlook.com>
2024-02-09 14:56:36 +0200

    Add Length of Last Word task solution

---
+++ TopInterview150/Tests/LengthLastWordTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests;
+
+internal class LengthLastWordTests
+{
+    [Test]
+    [TestCase("Hello World", 5)]
+    [TestCase("   fly me   to   the moon  ", 4)]
+    [TestCase("luffy is still joyboy", 6)]
+    public void Tests(string s, int expected)
+    {
+        //Arrange
+        var instance = new LengthLastWord();
+
+        //Act
+        int actual = instance.LengthOfLastWord(s);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/LengthLastWord.cs
@@ -0,0 +1,26 @@
+﻿namespace TopInterview150;
+
+public class LengthLastWord
+{
+    public int LengthOfLastWord(string s)
+    {
+        int end = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                end = i;
+                break;
+            }
+        }
+
+        int len = 1;
+        for (int i = end - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ') len++;
+            else break;
+        }
+
+        return len;
+    }
+}
