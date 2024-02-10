80e1dfccff23bb6e1cafabbb1166e9d5220dc968
Petar Belchev <petar.belchev@outlook.com>
2024-02-10 19:53:35 +0200

    Add Candy task solution

---
+++ TopInterview150/Tests/CandyTaskTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests;
+
+internal class CandyTaskTests
+{
+    [Test]
+    [TestCase(new int[] { 1, 0, 2 }, 5)]
+    [TestCase(new int[] { 1, 2, 2 }, 4)]
+    [TestCase(new int[] { 1, 2, 87, 87, 87, 2, 1 }, 13)]
+    [TestCase(new int[] { 1, 2, 3, 1, 0 }, 9)]
+    public void Tests(int[] ratings, int expected)
+    {
+        //Arrange
+        var instance = new CandyTask();
+
+        //Act
+        int actual = instance.Candy(ratings);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/CandyTask.cs
@@ -0,0 +1,33 @@
+﻿namespace TopInterview150;
+
+public class CandyTask
+{
+    public int Candy(int[] ratings)
+    {
+        int[] candies = Enumerable.Repeat(1, ratings.Length).ToArray();
+        var stack = new Stack<int>();
+
+        for (int child = 1; child < ratings.Length; child++)
+        {
+            if (ratings[child] > ratings[child - 1])
+                candies[child] = candies[child - 1] + 1;
+            else if (ratings[child] < ratings[child - 1] && candies[child - 1] == 1)
+                stack.Push(child - 1);
+        }
+
+        while (stack.Any())
+        {
+            int child = stack.Pop();
+            candies[child] = candies[child + 1] + 1;
+
+            if (child > 0
+                && ratings[child - 1] > ratings[child]
+                && candies[child - 1] <= candies[child])
+            {
+                stack.Push(child - 1);
+            }
+        }
+
+        return candies.Sum();
+    }
+}
