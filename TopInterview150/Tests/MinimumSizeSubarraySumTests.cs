6f81c0e3d28107387dc9ee353a8c5a60b64ea61a
Petar Belchev <petar.belchev@outlook.com>
2024-02-10 14:58:07 +0200

    Add Minimum Size Subarray Sum

---
+++ TopInterview150/Tests/MinimumSizeSubarraySumTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests;
+
+internal class MinimumSizeSubarraySumTests
+{
+    [Test]
+    [TestCase(7, new int[] { 2, 3, 1, 2, 4, 3 }, 2)]
+    [TestCase(4, new int[] { 1, 4, 4 }, 1)]
+    [TestCase(11, new int[] { 1, 1, 1, 1, 1, 1, 1, 1, }, 0)]
+    public void Tests(int target, int[] nums, int expected)
+    {
+        //Arrange
+        var instance = new MinimumSizeSubarraySum();
+
+        //Act
+        int actual = instance.MinSubArrayLen(target, nums);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/MinimumSizeSubarraySum.cs
@@ -0,0 +1,24 @@
+﻿namespace TopInterview150;
+
+public class MinimumSizeSubarraySum
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        int left = 0;
+        int minSize = int.MaxValue;
+        int sum = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            sum += nums[right];
+            
+            while (sum >= target)
+            {
+                minSize = Math.Min(minSize, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+
+        return minSize < int.MaxValue ? minSize : 0;
+    }
+}
