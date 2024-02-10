c7318ddab1c7a9702c878239eaf535794552e015
Petar Belchev <petar.belchev@outlook.com>
2024-02-10 10:43:01 +0200

    Add Two Sum II - Input Array is Sorted task solution

---
+++ TopInterview150/Tests/TwoSumII-InputArrayIsSortedTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests;
+
+internal class TwoSumII_InputArrayIsSortedTests
+{
+    [Test]
+    [TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 1, 2 })]
+    [TestCase(new int[] { 2, 3, 4 }, 6, new int[] { 1, 3 })]
+    [TestCase(new int[] { -1, 0 }, -1, new int[] { 1, 2 })]
+    public void Tests(int[] numbers, int target, int[] expected)
+    {
+        //Arrange
+        var instance = new TwoSumII_InputArrayIsSorted();
+
+        //Act
+        int[] actual = instance.TwoSum(numbers, target);
+
+        //Assert
+        Assert.That(string.Join(", ", actual), 
+            Is.EqualTo(string.Join(", ", expected)));
+    }
+}
---
+++ TopInterview150/TopInterview150/TwoSumII-InputArrayIsSorted.cs
@@ -0,0 +1,37 @@
+﻿namespace TopInterview150;
+
+public class TwoSumII_InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        for (int i1 = 0; i1 < numbers.Length - 1; i1++)
+        {
+            if (i1 > 0 && numbers[i1] == numbers[i1 - 1])
+                continue;
+
+            int i2 = FindNumberIndex(numbers, i1, target - numbers[i1]);
+
+            if (i2 != -1)
+                return new int[] { i1 + 1, i2 + 1 };
+        }
+
+        return Array.Empty<int>();
+    }
+
+    static int FindNumberIndex(int[] numbers, int offset, int number)
+    {
+        int left = offset + 1;
+        int right = numbers.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + ((right - left) / 2);
+
+            if (numbers[mid] < number) left = mid + 1;
+            else if (numbers[mid] > number) right = mid - 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+}
