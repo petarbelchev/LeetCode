d7bb788269eabb2ec634548fa5a57b9255e24d81
Petar Belchev <petar.belchev@outlook.com>
2024-02-10 11:37:55 +0200

    Add Container With Most Water task solution

---
+++ TopInterview150/TopInterview150/ContainerWithMostWater.cs
@@ -0,0 +1,32 @@
+﻿namespace TopInterview150;
+
+internal class ContainerWithMostWater
+{
+    public int MaxArea(int[] height)
+    {
+        int l = 0;
+        int r = height.Length - 1;
+        int max = 0;
+
+        while (l < r)
+        {
+            int v;
+            int len = r - l;
+
+            if (height[l] <= height[r])
+            {
+                v = len * height[l];
+                l++;
+            }
+            else
+            {
+                v = len * height[r];
+                r--;
+            }
+
+            if (max < v) max = v;
+        }
+
+        return max;
+    }
+}
