b831e17aab4d271aba4baa7f6727607d5e5774a5
Petar Belchev <petar.belchev@outlook.com>
2024-02-12 15:11:37 +0200

    Add Integer to Roman task solution

---
+++ TopInterview150/TopInterview150/IntegerToRoman.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace TopInterview150;
+
+internal class IntegerToRoman
+{
+    readonly IEnumerable<(int Value, string Number)> _romanNumbers = new[]
+    {
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
+    };
+
+    public string IntToRoman(int num)
+    {
+        var sb = new StringBuilder();
+
+        while (num > 0)
+        {
+            var (Value, Number) = _romanNumbers.First(n => n.Value <= num);
+            num -= Value;
+            sb.Append(Number);
+        }
+
+        return sb.ToString();
+    }
+}
