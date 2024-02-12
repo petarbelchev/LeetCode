689217cbffcdc31b7ee10386d77549b4b7a8f0f7
Petar Belchev <petar.belchev@outlook.com>
2024-02-12 14:16:44 +0200

    Add Trapping Rain Water task solution

---
+++ TopInterview150/Tests/TrappingRainWaterTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests;
+
+internal class TrappingRainWaterTests
+{
+    [Test]
+    [TestCase(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6)]
+    [TestCase(new int[] { 4, 2, 0, 3, 2, 5 }, 9)]
+    [TestCase(new int[] { 5, 4, 1, 2 }, 1)]
+    public void Tests(int[] height, int expected)
+    {
+        // Arrange
+        var instance = new TrappingRainWater();
+
+        // Act
+        int actual = instance.Trap(height);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/TrappingRainWater.cs
@@ -0,0 +1,39 @@
+﻿namespace TopInterview150;
+
+public class TrappingRainWater
+{
+    public int Trap(int[] height)
+    {
+        int water = 0;
+        int highIdx = 0;
+        int lowIdx = 0;
+
+        for (int barIdx = 1; barIdx < height.Length; barIdx++)
+        {
+            if (height[lowIdx] < height[barIdx])
+            {
+                for (int i = barIdx - 1; i > highIdx; i--)
+                {
+                    int waterToFill = Math.Min(height[barIdx], height[highIdx]) - height[i];
+                 
+                    if (waterToFill <= 0)
+                        break;
+                    
+                    height[i] += waterToFill;
+                    water += waterToFill;
+                }
+
+                if (height[highIdx] <= height[barIdx])
+                    highIdx = barIdx;
+                
+                lowIdx = barIdx;
+            }
+            else if (height[lowIdx] > height[barIdx])
+            {
+                lowIdx = barIdx;
+            }
+        }
+
+        return water;
+    }
+}
