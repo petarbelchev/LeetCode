5320ff6ea1dff62501191ff6536b191294b28ef5
Petar Belchev <petar.belchev@outlook.com>
2024-02-12 19:06:26 +0200

    Add Longest Common Prefix task solution

---
+++ TopInterview150/TopInterview150/LongestCommonPrefixTask.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace TopInterview150;
+
+internal class LongestCommonPrefixTask
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 1)
+            return strs[0];
+
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (strs[j].Length <= i || strs[j][i] != strs[0][i])
+                    return sb.ToString();
+            }
+
+            sb.Append(strs[0][i]);
+        }
+
+        return sb.ToString();
+    }
+}
