ece850319b37d5864f8ce73750af9cdad75471f3
Petar Belchev <petar.belchev@outlook.com>
2024-02-12 20:17:11 +0200

    Add Reverse Words in a String task solution

---
+++ TopInterview150/TopInterview150/ReverseWordsInString.cs
@@ -0,0 +1,7 @@
+﻿namespace TopInterview150;
+
+public class ReverseWordsInString
+{
+    public string ReverseWords(string s)
+        => string.Join(' ', s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+}
