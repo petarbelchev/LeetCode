330727928cfcf76c89445e61f1eda20738ff6027
Petar Belchev <petar.belchev@outlook.com>
2024-02-14 12:16:37 +0200

    Add Valid Sudoku task solution

---
+++ TopInterview150/Tests/ValidSudokuTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Tests;
+
+internal class ValidSudokuTests
+{
+    [Test]
+    [TestCaseSource(nameof(ValidSudokuCases))]
+    public void Tests((char[][] Board, bool Expected) testCase)
+    {
+        // Arrange
+        var instance = new ValidSudoku();
+
+        // Act
+        bool actual = instance.IsValidSudoku(testCase.Board);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(testCase.Expected));
+    }
+
+    static IEnumerable<(char[][] Board, bool Expected)> ValidSudokuCases()
+    {
+        yield return (new char[][]
+        {
+            new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+            new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+            new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+            new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+            new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+            new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+            new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+            new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+            new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
+        }, true);
+
+        yield return (new char[][]
+        {
+            new char[] { '8', '3', '.', '.', '7', '.', '.', '.', '.' },
+            new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+            new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+            new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+            new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+            new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+            new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+            new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+            new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' },
+        }, false);
+    }
+}
---
+++ TopInterview150/TopInterview150/ValidSudoku.cs
@@ -0,0 +1,29 @@
+﻿namespace TopInterview150;
+
+public class ValidSudoku
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        var validator = new HashSet<string>();
+
+        for (int row = 0; row < board.Length; row++)
+        {
+            for (int col = 0; col < board.Length; col++)
+            {
+                if (board[row][col] != '.')
+                {
+                    string value = '(' + board[row][col] + ")";
+
+                    if (validator.Add(row + value) == false
+                        || validator.Add(value + col) == false
+                        || validator.Add((row / 3) + value + (col / 3)) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
