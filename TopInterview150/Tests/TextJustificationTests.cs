9526596190db1c1497b3de408822cd1b21b47ad1
Petar Belchev <petar.belchev@outlook.com>
2024-02-15 20:20:17 +0200

    Add Text Justification task solution

---
+++ TopInterview150/Tests/TextJustificationTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Tests;
+
+internal class TextJustificationTests
+{
+    [Test]
+    [TestCase(
+        new string[] { "This", "is", "an", "example", "of", "text", "justification." },
+        16,
+        new string[] { "This    is    an", "example  of text", "justification.  " })]
+    [TestCase(
+        new string[] { "What", "must", "be", "acknowledgment", "shall", "be" },
+        16,
+        new string[] { "What   must   be", "acknowledgment  ", "shall be        " })]
+    [TestCase(
+        new string[] { "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do" },
+        20,
+        new string[] { "Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  " })]
+    [TestCase(
+        new string[] { "The", "important", "thing", "is", "not", "to", "stop", "questioning.", "Curiosity", "has", "its", "own", "reason", "for", "existing." },
+        17,
+        new string[] { "The     important", "thing  is  not to", "stop questioning.", "Curiosity has its", "own   reason  for", "existing.        " })]
+    [TestCase(
+        new string[] { "Listen", "to", "many,", "speak", "to", "a", "few." },
+        6,
+        new string[] { "Listen", "to    ", "many, ", "speak ", "to   a", "few.  " })]
+    [TestCase(
+        new string[] { "Here", "is", "an", "example", "of", "text", "justification." },
+        15,
+        new string[] { "Here    is   an", "example of text", "justification. " })]
+    public void Tests(string[] words, int maxWidth, string[] expected)
+    {
+        // Arrange
+        var instance = new TextJustification();
+
+        // Act
+        IList<string> actual = instance.FullJustify(words, maxWidth);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            for (int i = 0; i < expected.Length; i++)
+                Assert.That(actual[i], Is.EqualTo(expected[i]));
+        });
+    }
+}
---
+++ TopInterview150/TopInterview150/TextJustification.cs
@@ -0,0 +1,63 @@
+﻿namespace TopInterview150;
+
+public class TextJustification
+{
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        var result = new List<string>();
+        var row = new Row(maxWidth);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (row.AddWord(words[i]) == false)
+            {
+                i--;
+                result.Add(row.GetRowData());
+                row = new(maxWidth);
+            }
+        }
+
+        result.Add(row.GetRowData(true));
+
+        return result;
+    }
+
+    class Row
+    {
+        readonly int _maxWidth;
+        int _length;
+        readonly List<string> _data = new();
+
+        public Row(int maxWidth) => _maxWidth = maxWidth;
+
+        int RequiredSpaces => _data.Count - 1;
+
+        public bool AddWord(string word)
+        {
+            if (_length + word.Length + _data.Count > _maxWidth)
+                return false;
+
+            _data.Add(word);
+            _length += word.Length;
+
+            return true;
+        }
+
+        public string GetRowData(bool isLastRow = false)
+        {
+            int spaces = _maxWidth - (_length + RequiredSpaces);
+
+            if (isLastRow || _data.Count == 1)
+                return string.Join(' ', _data) + new string(' ', spaces);
+
+            int i = 0;
+            while (spaces % RequiredSpaces != 0)
+            {
+                _data[i++] += ' ';
+                spaces--;
+            }
+
+            return string.Join(new string(' ', spaces / RequiredSpaces + 1), _data);
+        }
+    }
+}
