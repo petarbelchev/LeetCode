bf190f71fcc28d13000c045d6599813643f006ce
Petar Belchev <petar.belchev@outlook.com>
2024-02-16 21:08:19 +0200

    Add Valid Palindrome task solution

---
+++ TopInterview150/TopInterview150/ValidPalindrome.cs
@@ -0,0 +1,30 @@
+﻿namespace TopInterview150;
+
+class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        int l = 0;
+        int r = s.Length - 1;
+
+        while (l <= r)
+        {
+            if (char.IsLetterOrDigit(s[l]) == false)
+            {
+                l++;
+                continue;
+            }
+
+            if (char.IsLetterOrDigit(s[r]) == false)
+            {
+                r--;
+                continue;
+            }
+
+            if (char.ToLower(s[l++]) != char.ToLower(s[r--]))
+                return false;
+        }
+
+        return true;
+    }
+}
