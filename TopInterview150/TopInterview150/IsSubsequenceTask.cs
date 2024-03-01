c16e9da921c3b43682fc778206fde9be468f28e7
Petar Belchev <petar.belchev@outlook.com>
2024-03-01 08:45:17 +0200

    Add Is Subsequence task solution

---
+++ TopInterview150/TopInterview150/IsSubsequenceTask.cs
@@ -0,0 +1,25 @@
+﻿namespace TopInterview150;
+
+class IsSubsequenceTask
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int sIdx = 0;
+        int tIdx = 0;
+
+        while (tIdx < t.Length && sIdx < s.Length)
+        {
+            if (t[tIdx] == s[sIdx])
+            {
+                tIdx++;
+                sIdx++;
+            }
+            else
+            {
+                tIdx++;
+            }
+        }
+
+        return sIdx >= s.Length;
+    }
+}
