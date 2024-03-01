21ff23acc0fbf2998b200bc4a50782315e43c3da
Petar Belchev <petar.belchev@outlook.com>
2024-03-01 19:31:32 +0200

    Add 3Sum task solution

---
+++ TopInterview150/Tests/3SumTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Tests;
+
+internal class _3SumTests
+{
+    [Test]
+    [TestCase(new int[] { 0, 1, 1 })]
+    [TestCase(new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 })]
+    [TestCase(new int[] { -4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0 },
+        new int[] { -5, 1, 4 },
+        new int[] { -4, 0, 4 },
+        new int[] { -4, 1, 3 },
+        new int[] { -2, -2, 4 },
+        new int[] { -2, 1, 1 },
+        new int[] { 0, 0, 0 })]
+    public void Tests(int[] nums, params int[][] expected)
+    {
+        // Arrange
+        var instance = new _3Sum();
+
+        // Act
+        IList<IList<int>> actual = instance.ThreeSum(nums);
+
+        // Assert
+        if (expected == null)
+        {
+            Assert.That(actual, Has.Count.EqualTo(0));
+        }
+        else
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Has.Count.EqualTo(expected.Length));
+
+                for (int i = 0; i < expected.Length; i++)
+                {
+                    for (int j = 0; j < expected[i].Length; j++)
+                    {
+                        Assert.That(actual.Any(x => x.Contains(expected[i][j])), Is.True);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ TopInterview150/TopInterview150/3Sum.cs
@@ -0,0 +1,66 @@
+﻿namespace TopInterview150;
+
+public class _3Sum
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var result = new List<IList<int>>();
+        int start = 0;
+
+        while (start < nums.Length - 2)
+        {
+            int left = start + 1;
+            int right = nums.Length - 1;
+
+            if (nums[start] + nums[left] > 0)
+                break;
+
+            while (left != -1 && right != -1 && left < right)
+            {
+                int sum = nums[start] + nums[left] + nums[right];
+
+                if (sum < 0)
+                    left++;
+                else if (sum > 0)
+                    right--;
+                else
+                {
+                    result.Add(new List<int>() { nums[start], nums[left], nums[right] });
+                    int newLeft = TakeNextLeftIndex(left, right, nums);
+                    right = TakeNextRightIndex(left, right, nums);
+                    left = newLeft;
+                }
+            }
+
+            start = TakeNextLeftIndex(start, nums.Length - 1, nums);
+
+            if (start == -1)
+                break;
+        }
+
+        return result;
+    }
+
+    static int TakeNextLeftIndex(int left, int right, IList<int> nums)
+    {
+        for (int i = left + 1; i < right; i++)
+        {
+            if (nums[i] != nums[left])
+                return i;
+        }
+
+        return -1;
+    }
+
+    static int TakeNextRightIndex(int left, int right, IList<int> nums)
+    {
+        for (int i = right - 1; i > left; i--)
+        {
+            if (nums[i] != nums[right])
+                return i;
+        }
+
+        return -1;
+    }
+}
