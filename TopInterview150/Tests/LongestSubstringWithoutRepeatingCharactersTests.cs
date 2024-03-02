d2b7a96ba5cfda9d2aef30da4af2cf7e14dd07a6
Petar Belchev <petar.belchev@outlook.com>
2024-03-02 13:38:36 +0200

    Add Longest Substring Without Repeating Characters task solution

---
+++ TopInterview150/Tests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests;
+
+class LongestSubstringWithoutRepeatingCharactersTests
+{
+    [Test]
+    [TestCase("abcabcbb", 3)]
+    [TestCase("bbbbb", 1)]
+    [TestCase("pwwkew", 3)]
+    [TestCase("au", 2)]
+    [TestCase("aa", 1)]
+    [TestCase("aab", 2)]
+    [TestCase("dvdf", 3)]
+    [TestCase("bpfbhmipx", 7)]
+    public void Tests(string s, int expected)
+    {
+        // Arrange
+        var instance = new LongestSubstringWithoutRepeatingCharacters();
+
+        // Act
+        int actual = instance.LengthOfLongestSubstring(s);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,38 @@
+﻿namespace TopInterview150;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length == 0 || s.Length == 1)
+            return s.Length;
+
+        int l = 0;
+        var sequence = new Dictionary<char, int>() { { s[l], l } };
+        int maxSequenceLength = 1;
+
+        for (int r = 1; r < s.Length; r++)
+        {
+            if (sequence.TryGetValue(s[r], out int repeatedCharIndex))
+            {
+                while (s[repeatedCharIndex] == s[r] && repeatedCharIndex < r)
+                    repeatedCharIndex++;
+
+                for (int i = l; i < repeatedCharIndex; i++)
+                    sequence.Remove(s[i]);
+
+                l = repeatedCharIndex;
+                sequence.Add(s[r], r);
+            }
+            else
+            {
+                sequence.Add(s[r], r);
+
+                if (sequence.Count > maxSequenceLength)
+                    maxSequenceLength = sequence.Count;
+            }
+        }
+
+        return maxSequenceLength;
+    }
+}
