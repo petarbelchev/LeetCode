bc4ec4c5c54065f287b4027c9dd3a565d3297d99
Petar Belchev <petar.belchev@outlook.com>
2024-03-06 18:46:41 +0200

    Add Minimum Window Substring task solution

---
+++ TopInterview150/Tests/MinimumWindowSubstringTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests;
+
+class MinimumWindowSubstringTests
+{
+    [Test]
+    [TestCase("ADOBECODEBANC", "ABC", "BANC")]
+    [TestCase("a", "a", "a")]
+    [TestCase("a", "aa", "")]
+    [TestCase("aa", "aa", "aa")]
+    [TestCase("bbaa", "aba", "baa")]
+    [TestCase("bba", "ab", "ba")]
+    public void Tests(string s, string t, string expected)
+    {
+        // Arrange
+        var instance = new MinimumWindowSubstring();
+
+        // Act
+        string actual = instance.MinWindow(s, t);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/MinimumWindowSubstring.cs
@@ -0,0 +1,87 @@
+﻿namespace TopInterview150;
+
+public class MinimumWindowSubstring
+{
+    readonly int[] _required = new int[128];
+    readonly int[] _founded = new int[128];
+    int _length = int.MaxValue;
+    int _counter = 0;
+    int _left = 0;
+    int _right = 0;
+    int _minStart = 0;
+
+    public string MinWindow(string s, string t)
+    {
+        if (s.Length < t.Length)
+            return string.Empty;
+
+        SetRequiredChars(t);
+
+        while (_right < s.Length)
+        {
+            CheckRight(s);
+
+            while (ShouldCheckWindow(t))
+            {
+                CheckWindow();
+                MoveLeft(s);
+            }
+
+            _right++;
+        }
+
+        return GetMinWindow(s);
+    }
+
+    void SetRequiredChars(string t)
+    {
+        foreach (char ch in t)
+            _required[ch]++;
+    }
+
+    void CheckRight(string s)
+    {
+        char ch = s[_right];
+
+        if (_required[ch] > 0)
+        {
+            _founded[ch]++;
+
+            if (_required[ch] >= _founded[ch])
+                _counter++;
+        }
+    }
+
+    bool ShouldCheckWindow(string t) => _counter == t.Length;
+
+    void CheckWindow()
+    {
+        int currLength = _right + 1 - _left;
+
+        if (currLength < _length)
+        {
+            _length = currLength;
+            _minStart = _left;
+        }
+    }
+
+    void MoveLeft(string s)
+    {
+        do
+        {
+            char leftChar = s[_left++];
+
+            if (_required[leftChar] > 0)
+            {
+                _founded[leftChar]--;
+
+                if (_required[leftChar] > _founded[leftChar])
+                    _counter--;
+            }
+
+        } while (_left < _right && _required[s[_left]] == 0);
+    }
+
+    string GetMinWindow(string s)
+        => _length == int.MaxValue ? string.Empty : s.Substring(_minStart, _length);
+}
