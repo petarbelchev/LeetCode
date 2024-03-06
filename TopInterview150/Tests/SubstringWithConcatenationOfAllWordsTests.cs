a24125d7750e15da15279278199899da6c906207
Petar Belchev <petar.belchev@outlook.com>
2024-03-06 11:01:54 +0200

    Add Substring With Concatenation Of All Words task solution

---
+++ TopInterview150/Tests/SubstringWithConcatenationOfAllWordsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests;
+
+class SubstringWithConcatenationOfAllWordsTests
+{
+    [Test]
+    [TestCase("barfoothefoobarman", new string[] { "foo", "bar" }, new int[] { 0, 9 })]
+    [TestCase("wordgoodgoodgoodbestword", new string[] { "word", "good", "best", "word" }, null)]
+    [TestCase("barfoofoobarthefoobarman", new string[] { "bar", "foo", "the" }, new int[] { 6, 9, 12 })]
+    [TestCase("ababababab", new string[] { "ababa", "babab" }, new int[] { 0 })]
+    [TestCase("lingmindraboofooowingdingbarrwingmonkeypoundcake", new string[] { "fooo", "barr", "wing", "ding", "wing" }, new int[] { 13 })]
+    [TestCase("aaaaaaaaaaaaaa", new string[] { "aa", "aa" }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+    public void Tests(string s, string[] words, int[] expected)
+    {
+        // Arrange
+        var instance = new SubstringWithConcatenationOfAllWords();
+        expected ??= Array.Empty<int>();
+
+        // Act
+        IList<int> actual = instance.FindSubstring(s, words);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Has.Count.EqualTo(expected.Length));
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.That(actual.Contains(expected[i]), Is.True);
+        });
+    }
+}
---
+++ TopInterview150/TopInterview150/SubstringWithConcatenationOfAllWords.cs
@@ -0,0 +1,67 @@
+﻿namespace TopInterview150;
+
+public class SubstringWithConcatenationOfAllWords
+{
+    public IList<int> FindSubstring(string s, string[] words)
+    {
+        int wordsLength = words[0].Length;
+        var indices = new List<int>();
+
+        if (s.Length < words.Length * wordsLength)
+            return indices;
+
+        var wordsFound = new Dictionary<string, int>();
+        foreach (string word in words)
+        {
+            if (wordsFound.ContainsKey(word) == false)
+                wordsFound[word] = 0;
+
+            wordsFound[word]++;
+        }
+
+        var wordsCount = new Dictionary<string, int>();
+
+        for (int i = 0; i < wordsLength; i++)
+        {
+            int left = i;
+            int right = i;
+            int currentWordsCount = 0;
+
+            while (right <= s.Length - wordsLength)
+            {
+                string word = s.Substring(right, wordsLength);
+                right += wordsLength;
+
+                if (wordsFound.ContainsKey(word))
+                {
+                    if (!wordsCount.ContainsKey(word))
+                        wordsCount[word] = 0;
+
+                    wordsCount[word]++;
+                    currentWordsCount++;
+
+                    while (wordsFound[word] < wordsCount[word])
+                    {
+                        string leftWord = s.Substring(left, wordsLength);
+                        wordsCount[leftWord]--;
+                        currentWordsCount--;
+                        left += wordsLength;
+                    }
+
+                    if (currentWordsCount == words.Length)
+                        indices.Add(left);
+                }
+                else
+                {
+                    left = right;
+                    wordsCount.Clear();
+                    currentWordsCount = 0;
+                }
+            }
+
+            wordsCount.Clear();
+        }
+
+        return indices;
+    }
+}
