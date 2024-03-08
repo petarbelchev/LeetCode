e3eaf1115c47f21feabc3270861600895d5bcc3b
Petar Belchev <petar.belchev@outlook.com>
2024-03-08 09:33:57 +0200

    Add Spiral Matrix task solution

---
+++ TopInterview150/Tests/SpiralMatrixTests.cs
@@ -0,0 +1,80 @@
+﻿namespace Tests;
+
+internal class SpiralMatrixTests
+{
+    [Test]
+    [TestCaseSource(nameof(Cases))]
+    public void Tests((int[][] Matrix, int[] Expected) testCase)
+    {
+        // Arrange
+        var instance = new SpiralMatrix();
+
+        // Act
+        IList<int> actual = instance.SpiralOrder(testCase.Matrix);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Has.Count.EqualTo(testCase.Expected.Length));
+
+            for (int i = 0; i < testCase.Expected.Length; i++)
+                Assert.That(actual[i], Is.EqualTo(testCase.Expected[i]));
+        });
+    }
+
+    static IEnumerable<(int[][] Matrix, int[] Expected)> Cases()
+    {
+        yield return (
+            new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 },
+                new int[] { 7, 8, 9 },
+            },
+            new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 });
+
+        yield return (
+            new int[][]
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 5, 6, 7, 8 },
+                new int[] { 9, 10, 11, 12 },
+            },
+            new int[] { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 });
+
+        yield return (
+            new int[][]
+            {
+                new int[] { 1 }
+            },
+            new int[] { 1 });
+
+        yield return (
+            new int[][]
+            {
+                new int[] { 7 },
+                new int[] { 9 },
+                new int[] { 6 },
+            },
+            new int[] { 7, 9, 6 });
+
+        yield return (
+            new int[][]
+            {
+                new int[] { 2, 5, 8 },
+                new int[] { 4, 0, -1 },
+            },
+            new int[] { 2, 5, 8, -1, 0, 4 });
+
+        yield return (
+            new int[][]
+            {
+                new int[] { 2, 3, 4 },
+                new int[] { 5, 6, 7 },
+                new int[] { 8, 9, 10 },
+                new int[] { 11, 12, 13 },
+                new int[] { 14, 15, 16 },
+            },
+            new int[] { 2, 3, 4, 7, 10, 13, 16, 15, 14, 11, 8, 5, 6, 9, 12 });
+    }
+}
---
+++ TopInterview150/TopInterview150/SpiralMatrix.cs
@@ -0,0 +1,90 @@
+﻿namespace TopInterview150;
+
+public class SpiralMatrix
+{
+    int _top = 0;
+    int _left = 0;
+    int _right;
+    int _bottom;
+
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        _right = matrix[0].Length - 1;
+        _bottom = matrix.Length - 1;
+
+        if (_bottom == 0)
+            return GetTop(matrix);
+
+        if (_right == 0)
+            return GetRight(matrix);
+
+        var output = new List<int>();
+
+        while (true)
+        {
+            output.AddRange(GetTop(matrix));
+
+            if (BottomPassTopOrLeftPassRight()) break;
+
+            output.AddRange(GetRight(matrix));
+            output.AddRange(GetBottom(matrix));
+
+            if (BottomPassTopOrLeftPassRight()) break;
+
+            output.AddRange(GetLeft(matrix));
+        }
+
+        return output;
+    }
+
+    IList<int> GetBottom(int[][] matrix)
+    {
+        var output = new List<int>();
+
+        for (int col = _right; col >= _left; col--)
+            output.Add(matrix[_bottom][col]);
+
+        _bottom--;
+
+        return output;
+    }
+
+    IList<int> GetRight(int[][] matrix)
+    {
+        var output = new List<int>();
+
+        for (int row = _top; row <= _bottom; row++)
+            output.Add(matrix[row][_right]);
+
+        _right--;
+
+        return output;
+    }
+
+    IList<int> GetTop(int[][] matrix)
+    {
+        var output = new List<int>();
+
+        for (int col = _left; col <= _right; col++)
+            output.Add(matrix[_top][col]);
+
+        _top++;
+
+        return output;
+    }
+
+    IList<int> GetLeft(int[][] matrix)
+    {
+        var output = new List<int>();
+
+        for (int row = _bottom; row >= _top; row--)
+            output.Add(matrix[row][_left]);
+
+        _left++;
+
+        return output;
+    }
+
+    bool BottomPassTopOrLeftPassRight()
+        => _bottom < _top || _left > _right;
+}
