5116785e5d18f699f08d3b63b6f16530df0cfeb6
Petar Belchev <petar.belchev@outlook.com>
2024-03-09 15:47:02 +0200

    Add Rotate Image task solution

---
+++ TopInterview150/Tests/RotateImageTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Tests;
+
+class RotateImageTests
+{
+    int[][][] matrixCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 1, 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9 },
+        },
+        new int[][]
+        {
+            new int[] { 5, 1, 9, 11 },
+            new int[] { 2, 4, 8, 10 },
+            new int[] { 13, 3, 6, 7 },
+            new int[] { 15, 14, 12, 16 },
+        }
+    };
+
+    int[][][] expectedCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 7, 4, 1 },
+            new int[] { 8, 5, 2 },
+            new int[] { 9, 6, 3 },
+        },
+        new int[][]
+        {
+            new int[] { 15, 13, 2, 5 },
+            new int[] { 14, 3, 4, 1 },
+            new int[] { 12, 6, 8, 9 },
+            new int[] { 16, 7, 10, 11 },
+        }
+    };
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    public void Tests(int caseNumber)
+    {
+        // Arrange
+        int[][] actual = matrixCases[caseNumber];
+        int[][] expected = expectedCases[caseNumber];
+        var instance = new RotateImage();
+
+        // Act
+        instance.Rotate(actual);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Has.Length.EqualTo(expected.Length));
+
+            for (int row = 0; row < expected.Length; row++)
+            {
+                for (int col = 0; col < expected[row].Length; col++)
+                    Assert.That(actual[row][col], Is.EqualTo(expected[row][col]));
+            }
+        });
+    }
+}
---
+++ TopInterview150/TopInterview150/RotateImage.cs
@@ -0,0 +1,63 @@
+﻿namespace TopInterview150;
+
+public class RotateImage
+{
+    int _top, _right, _left, _bottom, _rightBottomBoundary, _leftTopBoundary;
+
+    public void Rotate(int[][] matrix)
+    {
+        if (matrix.Length == 1) return;
+
+        int cycles = (matrix.Length - 1) / 2;
+        for (int cycle = 0; cycle <= cycles; cycle++)
+        {
+            SetPointersAndBoundaries(matrix, cycle);
+
+            while (_top < _rightBottomBoundary)
+            {
+                int right = MoveTopToRight(matrix);
+                int bottom = MoveRightToBottom(right, matrix);
+                int left = MoveBottomToLeft(bottom, matrix);
+                MoveLeftToTop(left, matrix);
+            }
+        }
+    }
+
+    void SetPointersAndBoundaries(int[][] matrix, int cycle)
+    {
+        _top = cycle;
+        _right = cycle;
+        _left = matrix.Length - 1 - cycle;
+        _bottom = matrix.Length - 1 - cycle;
+
+        _leftTopBoundary = cycle;
+        _rightBottomBoundary = matrix.Length - 1 - cycle;
+    }
+
+    int MoveTopToRight(int[][] matrix)
+    {
+        int rightElem = matrix[_right][_rightBottomBoundary];
+        matrix[_right++][_rightBottomBoundary] = matrix[_leftTopBoundary][_top];
+
+        return rightElem;
+    }
+
+    int MoveRightToBottom(int right, int[][] matrix)
+    {
+        int bottomElem = matrix[_rightBottomBoundary][_bottom];
+        matrix[_rightBottomBoundary][_bottom--] = right;
+
+        return bottomElem;
+    }
+
+    int MoveBottomToLeft(int bottom, int[][] matrix)
+    {
+        int leftElem = matrix[_left][_leftTopBoundary];
+        matrix[_left--][_leftTopBoundary] = bottom;
+
+        return leftElem;
+    }
+
+    void MoveLeftToTop(int left, int[][] matrix)
+        => matrix[_leftTopBoundary][_top++] = left;
+}
