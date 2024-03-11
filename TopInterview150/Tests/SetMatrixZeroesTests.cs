be20bd6068e62a038a17d091000342a7aaed62c3
Petar Belchev <petar.belchev@outlook.com>
2024-03-11 17:29:56 +0200

    Add Set Matrix Zaroes task solution

---
+++ TopInterview150/Tests/SetMatrixZeroesTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests;
+
+internal class SetMatrixZeroesTests
+{
+    int[][][] matrixCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 1, 1, 1 },
+            new int[] { 1, 0, 1 },
+            new int[] { 1, 1, 1 },
+        },
+        new int[][]
+        {
+            new int[] { 0, 1, 2, 0 },
+            new int[] { 3, 4, 5, 2 },
+            new int[] { 1, 3, 1, 5 }
+        },
+        new int[][]
+        {
+            new int[] { 1 },
+            new int[] { 0 }
+        }
+    };
+
+    int[][][] expectedCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 1, 0, 1 },
+            new int[] { 0, 0, 0 },
+            new int[] { 1, 0, 1 },
+        },
+        new int[][]
+        {
+            new int[] { 0, 0, 0, 0 },
+            new int[] { 0, 4, 5, 0 },
+            new int[] { 0, 3, 1, 0 }
+        },
+        new int[][]
+        {
+            new int[] { 0 },
+            new int[] { 0 },
+        }
+    };
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    public void Tests(int caseNumber)
+    {
+        // Arrange
+        int[][] actual = matrixCases[caseNumber];
+        int[][] expected = expectedCases[caseNumber];
+        var instance = new SetMatrixZeroes();
+
+        // Act
+        instance.SetZeroes(actual);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Has.Length.EqualTo(expected.Length));
+
+            for (int row = 0; row < expected.Length; row++)
+            {
+                for (int col = 0; col < expected[row].Length; col++)
+                    Assert.That(actual[row][col], Is.EqualTo(expected[row][col]));
+            }
+        });
+    }
+}
---
+++ TopInterview150/TopInterview150/SetMatrixZeroes.cs
@@ -0,0 +1,86 @@
+﻿namespace TopInterview150;
+
+public class SetMatrixZeroes
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        bool shouldSetZerosToFirstRow = CheckFirstRow(matrix);
+
+        if (matrix.Length == 1)
+        {
+            if (shouldSetZerosToFirstRow) SetFirstRowZeros(matrix);
+
+            return;
+        }
+
+        bool shouldSetZerosToFirstCol = CheckFirstCol(matrix);
+
+        if (matrix[0].Length == 1)
+        {
+            if (shouldSetZerosToFirstCol) SetFirstColZeros(matrix);
+
+            return;
+        }
+
+        CheckForZeros(matrix);
+        SetZeros(matrix);
+
+        if (shouldSetZerosToFirstRow) SetFirstRowZeros(matrix);
+        if (shouldSetZerosToFirstCol) SetFirstColZeros(matrix);
+    }
+
+    static bool CheckFirstRow(int[][] matrix)
+    {
+        for (int col = 0; col < matrix[0].Length; col++)
+            if (matrix[0][col] == 0) return true;
+
+        return false;
+    }
+
+    static bool CheckFirstCol(int[][] matrix)
+    {
+        for (int row = 0; row < matrix.Length; row++)
+            if (matrix[row][0] == 0) return true;
+
+        return false;
+    }
+
+    static void SetFirstRowZeros(int[][] matrix)
+    {
+        for (int col = 0; col < matrix[0].Length; col++)
+            matrix[0][col] = 0;
+    }
+
+    static void SetFirstColZeros(int[][] matrix)
+    {
+        for (int row = 0; row < matrix.Length; row++)
+            matrix[row][0] = 0;
+    }
+
+    static void CheckForZeros(int[][] matrix)
+    {
+        for (int row = 1; row < matrix.Length; row++)
+        {
+            for (int col = 1; col < matrix[row].Length; col++)
+            {
+                if (matrix[row][col] == 0)
+                {
+                    matrix[0][col] = 0;
+                    matrix[row][0] = 0;
+                }
+            }
+        }
+    }
+
+    static void SetZeros(int[][] matrix)
+    {
+        for (int row = 1; row < matrix.Length; row++)
+        {
+            for (int col = 1; col < matrix[row].Length; col++)
+            {
+                if (matrix[row][0] == 0 || matrix[0][col] == 0)
+                    matrix[row][col] = 0;
+            }
+        }
+    }
+}
