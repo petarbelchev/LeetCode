a29d1653dda19d388793903e39d452c8daf84a93
Petar Belchev <petar.belchev@outlook.com>
2024-03-12 11:19:01 +0200

    Add Game of Live task solution

---
+++ TopInterview150/Tests/GameOfLifeTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace Tests;
+
+internal class GameOfLifeTests
+{
+    int[][][] matrixCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 0,1,0 },
+            new int[] { 0,0,1 },
+            new int[] { 1,1,1 },
+            new int[] { 0,0,0 }
+        },
+        new int[][]
+        {
+            new int[] { 1,1 },
+            new int[] { 1,0 }
+        }
+    };
+
+    int[][][] expectedCases = new int[][][]
+    {
+        new int[][]
+        {
+            new int[] { 0,0,0 },
+            new int[] { 1,0,1 },
+            new int[] { 0,1,1 },
+            new int[] { 0,1,0 }
+        },
+        new int[][]
+        {
+            new int[] { 1, 1 },
+            new int[] { 1, 1 }
+        }
+    };
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    public void Tests(int caseNumber)
+    {
+        // Arrange
+        int[][] actual = matrixCases[caseNumber];
+        int[][] expected = expectedCases[caseNumber];
+        var instance = new GameOfLifeTask();
+
+        // Act
+        instance.GameOfLife(actual);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Has.Length.EqualTo(expected.Length));
+
+            for (int row = 0; row < expected.Length; row++)
+            {
+                for (int col = 0; col < expected[row].Length; col++)
+                    Assert.That(actual[row][col], Is.EqualTo(expected[row][col]));
+            }
+        });
+    }
+}
---
+++ TopInterview150/TopInterview150/GameOfLifeTask.cs
@@ -0,0 +1,80 @@
+﻿namespace TopInterview150;
+
+public class GameOfLifeTask
+{
+    public void GameOfLife(int[][] board)
+    {
+        for (int row = 0; row < board.Length; row++)
+        {
+            for (int col = 0; col < board[row].Length; col++)
+            {
+                int liveNeighborsCount = GetLiveNeighborsCount(row, col, board);
+                DecideLiveOrDead(row, col, board, liveNeighborsCount);
+            }
+        }
+
+        MakeChanges(board);
+    }
+
+    private int GetLiveNeighborsCount(int row, int col, int[][] board)
+    {
+        int count = 0;
+
+        for (int rowIdx = row - 1;
+            rowIdx <= row + 1 && rowIdx < board.Length;
+            rowIdx++)
+        {
+            if (rowIdx < 0)
+                continue;
+
+            for (int colIdx = col - 1;
+                colIdx <= col + 1 && colIdx < board[row].Length;
+                colIdx++)
+            {
+                if (colIdx < 0 || (rowIdx == row && colIdx == col))
+                    continue;
+
+                if (board[rowIdx][colIdx] == 1 || board[rowIdx][colIdx] == 2)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void DecideLiveOrDead(
+        int row,
+        int col,
+        int[][] board,
+        int liveNeighborsCount)
+    {
+        if (board[row][col] == 1)
+        {
+            if (liveNeighborsCount < 2 || liveNeighborsCount > 3)
+                board[row][col] = 2; // die
+        }
+        else
+        {
+            if (liveNeighborsCount == 3)
+                board[row][col] = 3; // live
+        }
+    }
+
+    private void MakeChanges(int[][] board)
+    {
+        for (int row = 0; row < board.Length; row++)
+        {
+            for (int col = 0; col < board[row].Length; col++)
+            {
+                if (board[row][col] == 2)
+                {
+                    board[row][col] = 0;
+                }
+                else if (board[row][col] == 3)
+                {
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+}
