18c22a8ec1df08d99ad58bc8af643f7e88f39556
Petar Belchev <petar.belchev@outlook.com>
2024-03-13 13:38:12 +0200

    Add Contains Duplicate II

---
+++ TopInterview150/TopInterview150/ContainsDuplicateII.cs
@@ -0,0 +1,23 @@
+﻿namespace TopInterview150;
+
+public class ContainsDuplicateII
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        if (nums.Length == 1) return false;
+
+        var tracker = new Dictionary<int, int>();
+
+        for (int currIdx = 0; currIdx < nums.Length; currIdx++)
+        {
+            if (tracker.TryGetValue(nums[currIdx], out int lastIdx))
+            {
+                if (currIdx - lastIdx <= k) return true; 
+            }
+
+            tracker[nums[currIdx]] = currIdx;
+        }
+
+        return false;
+    }
+}
