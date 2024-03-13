467d2134d7028fd75f45df2515d93505fe56b909
Petar Belchev <petar.belchev@outlook.com>
2024-03-13 13:16:13 +0200

    Add Two Sum

---
+++ TopInterview150/TopInterview150/TwoSumTask.cs
@@ -0,0 +1,19 @@
+﻿namespace TopInterview150;
+
+public class TwoSumTask
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var seen = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (seen.TryGetValue(target - nums[i], out int j))
+                return new int[] { i, j };
+
+            seen[nums[i]] = i;
+        }
+
+        return Array.Empty<int>();
+    }
+}
