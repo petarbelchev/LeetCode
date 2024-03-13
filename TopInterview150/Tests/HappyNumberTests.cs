def4b54ce9ecd286247cfb818681f6c02704a5b0
Petar Belchev <petar.belchev@outlook.com>
2024-03-13 13:15:56 +0200

    Add Happy Number

---
+++ TopInterview150/Tests/HappyNumberTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests;
+
+internal class HappyNumberTests
+{
+    [Test]
+    [TestCase(19, true)]
+    [TestCase(2, false)]
+    public void Tests(int n, bool expected)
+    {
+        // Arrange
+        var instance = new HappyNumber();
+
+        // Act
+        bool actual = instance.IsHappy(n);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ TopInterview150/TopInterview150/HappyNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace TopInterview150;
+
+public class HappyNumber
+{
+    public bool IsHappy(int n)
+    {
+        if (n == 1) return true;
+
+        var sums = new HashSet<int>();
+
+        while (true)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int m = n % 10;
+                n /= 10;
+                sum += m * m;
+            }
+
+            if (sum == 1) return true;
+
+            if (!sums.Add(sum)) return false;
+
+            n = sum;
+        }
+    }
+}
