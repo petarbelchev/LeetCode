49e823c2579e4972cf046f92a43c6c6338448387
Petar Belchev <petar.belchev@outlook.com>
2024-03-14 11:14:20 +0200

    Add Longest Consecutive Sequence

---
+++ TopInterview150/TopInterview150/LongestConsecutiveSequence.cs
@@ -0,0 +1,31 @@
+﻿namespace TopInterview150;
+
+public class LongestConsecutiveSequence
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+        var numSet = new HashSet<int>(nums);
+        int longestStreak = 0;
+
+        foreach (int num in numSet)
+        {
+            if (!numSet.Contains(num - 1))
+            {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.Contains(currentNum + 1))
+                {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.Max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
