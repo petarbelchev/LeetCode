7aa8457dbeb69c965f6bc27dcb461018d0f0ef1a
Petar Belchev <petar.belchev@outlook.com>
2024-03-16 13:30:54 +0200

    Add Linked List Cycle

---
+++ TopInterview150/TopInterview150/LinkedListCycle.cs
@@ -0,0 +1,35 @@
+﻿namespace TopInterview150;
+
+public class LinkedListCycle
+{
+    public bool HasCycle(ListNode head)
+    {
+        if (head is null) return false;
+
+        ListNode first = head;
+        ListNode second = head;
+
+        while (first.next is not null
+            && second.next?.next is not null)
+        {
+            first = first.next;
+            second = second.next.next;
+
+            if (first == second) return true;
+        }
+
+        return false;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
