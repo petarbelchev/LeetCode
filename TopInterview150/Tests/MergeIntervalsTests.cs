b5baea3e14457768814818ff74690bf761661035
Petar Belchev <petar.belchev@outlook.com>
2024-04-02 16:51:52 +0300

    Add Merge Intervals

---
+++ TopInterview150/Tests/MergeIntervalsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Tests;
+
+internal class MergeIntervalsTests
+{
+    [Test]
+    [TestCase("[[1,3],[2,6],[8,10],[15,18]]", "[[1,6],[8,10],[15,18]]")]
+    [TestCase("[[1,4],[4,5]]", "[[1,5]]")]
+    [TestCase("[[1,4],[5,6]]", "[[1,4],[5,6]]")]
+    [TestCase("[[2,3],[4,5],[6,7],[8,9],[1,10]]", "[[1,10]]")]
+    public void Tests(string intervalsJson, string expectedJson)
+    {
+        // Arrange
+        int[][] intervals = JsonSerializer.Deserialize<int[][]>(intervalsJson)!;
+        var instance = new MergeIntervals();
+
+        // Act
+        int[][] actual = instance.Merge(intervals);
+        string actualJson = JsonSerializer.Serialize(actual);
+
+        // Assert
+        Assert.That(actualJson, Is.EqualTo(expectedJson));
+    }
+}
---
+++ TopInterview150/TopInterview150/MergeIntervals.cs
@@ -0,0 +1,28 @@
+﻿namespace TopInterview150;
+
+public class MergeIntervals
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        if (intervals.Length == 1) return intervals;
+
+        Array.Sort(intervals, (x, y) => x[0] - y[0]);
+        var merged = new List<int[]>();
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int[] left = intervals[i - 1];
+            int[] right = intervals[i];
+
+            if (left[1] >= right[0])
+            {
+                intervals[i] = new int[] { left[0], Math.Max(left[1], right[1]) };
+            }
+            else merged.Add(left);
+
+            if (i == intervals.Length - 1) merged.Add(intervals[i]);
+        }
+
+        return merged.ToArray();
+    }
+}
